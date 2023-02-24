566e4717d242cfdcf4843efd5c42ff46afd9b58d
Jade Fung <jade@fungj.net>
2023-02-23 18:20:38 -0800

    removed chart title since it's redundant w/ big WPM figure above

--- src/components/RunningWPM.tsx
+++ src/components/RunningWPM.tsx
@@ -7,11 +7,6 @@ const RunningWPM = ({ session }: { session: DataFrame }): ReactElement => {
   // blah
   const runningWPM = computeRunningWPM(session);
   const options = {
-    title: {
-      text: "words per minute",
-      textAlign: "left",
-      right: "center",
-    },
     xAxis: {
       // generate labels: just 1 through n number of WPM calculations we made
       data: Array.from(new Array(runningWPM.length)).map((_, i) => i + 1),
